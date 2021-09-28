abd72d8489b0c665d9fb4f21474b71c970802aab
Zhi Lu <zhilu@std.uestc.edu.cn>
2021-09-28 14:12:56 +0800

    add config files

---
+++ setup.py
@@ -0,0 +1,62 @@
+import codecs
+import os.path
+
+import setuptools
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+VERSION = get_version("outfit_datasets/__init__.py")
+
+with open("README.md", "r") as fh:
+    README = fh.read()
+
+requirements = [
+    "attrs",
+    "colorama",
+    "lmdb",
+    "numpy",
+    "opencv-python",
+    "pandas",
+    "pillow",
+    "pytorch-ignite",
+    "pyyaml",
+    "matplotlib",
+    "scikit-learn",
+    "scipy",
+    "tensorboard",
+    "torch",
+    "torchvision",
+    "tqdm",
+]
+
+setuptools.setup(
+    # Metadata
+    name="outfit-datasets",
+    version=VERSION,
+    author="Zhi Lu",
+    author_email="zhilu@std.uestc.edu.cn",
+    url="https://github.com/lzcn/outfit-datasets",
+    description="A collections of outfit datasets",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    python_requires=">=3.6",
+    # Package info
+    packages=setuptools.find_packages(exclude=("tests", "tests.*",)),
+    zip_safe=True,
+    install_requires=requirements,
+)
