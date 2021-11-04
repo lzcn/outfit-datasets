67bd63250bb13c43d46d3cc13aa75d4276a19f40
Zhi Lu <zhilu@std.uestc.edu.cn>
2021-11-05 00:06:03 +0800

    refactor scripts

--- ifashion/downloader.py
+++ ifashion/downloader.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 import argparse
-import json
 import os
+import random
 import time
 from multiprocessing import Pool
-import random
+
 import requests
-from tqdm.auto import tqdm
+import torchutils
+from tqdm import tqdm
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
 }
 
 
-def download_image(img_path, img_link):
+def download_image(img_path, img_link, tmp_file):
     if os.path.exists(img_path):
         return
     while True:
@@ -25,7 +26,7 @@ def download_image(img_path, img_link):
                 file.write(response.content)
                 file.close()
             else:
-                with open("processed/broken_url.txt", "a") as f:
+                with open(tmp_file, "a") as f:
                     f.write(f"{img_link}\n")
                 tqdm.write(f"Failed. {img_link}")
             break
@@ -35,26 +36,26 @@ def download_image(img_path, img_link):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Merge all JSON file", formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-    )
-    parser.add_argument("--txt-file", default="raw/item_data.txt")
-    parser.add_argument("--image-dir", default="images")
-    parser.add_argument("--num-workers", default=16, type=int)
+    # fmt: off
+    parser = argparse.ArgumentParser(description="iFashon image downloader", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--data-file", default="release/item_data.txt", help="Path to item data file")
+    parser.add_argument("--image-dir", default="images", help="Path to save image")
+    parser.add_argument("--num-workers", default=16, type=int, help="Number of processers to use")
+    # fmt: on
     args = parser.parse_args()
-    # load urls
-    print("Reading urls")
-    if os.path.exists("processed/broken_url.txt"):
-        with open("processed/broken_url.txt") as f:
+    print("Reading item urls ...")
+    tmp_file = "broken_urls.txt"
+    if os.path.exists(tmp_file):
+        with open(tmp_file) as f:
             broken_url = f.read()
             broken_url = broken_url.split("\n")
     else:
         broken_url = []
     broken_url = set(broken_url)
-    # downloaded = set(torchutils.files.scan_files(args.image_dir, recursive=True))
+    downloaded = set(torchutils.files.scan_files(args.image_dir))
     todownload = []
     item_set = set()
-    with open(args.txt_file) as f:
+    with open(args.data_file) as f:
         for line in f:
             item_id, _, url, *title = line.strip().split(",")
             if item_id in item_set:
@@ -72,7 +73,7 @@ if __name__ == "__main__":
             todownload.append((img_path, url))
     print("Number of all items: {:,}".format(len(todownload) + len(broken_url)))
     print("Number of items to download: {:,}".format(len(todownload)))
-    # print("Number of items downloaded: {:,}".format(len(downloaded)))
+    print("Number of items downloaded: {:,}".format(len(downloaded)))
     print("Number of broken items: {:,}".format(len(broken_url)))
 
     pbar = tqdm(total=len(todownload))
@@ -80,12 +81,12 @@ if __name__ == "__main__":
     def update(*a):
         pbar.update()
 
-    tqdm.write("Adding tasks to pool.")
+    tqdm.write("Adding tasks to pool ...")
     pool = Pool(args.num_workers)
     for img_path, img_url in todownload:
-        pool.apply_async(download_image, args=(img_path, img_url), callback=update)
+        pool.apply_async(download_image, args=(img_path, img_url, tmp_file), callback=update)
     tqdm.write("All tasks are scheduled.")
     pool.close()
     pool.join()
-    tqdm.write("Finished. Please delete cache file for broken urls and re-run to check!")
+    tqdm.write("Finished. You can now delete the cache file for broken urls and re-run to check!")
     exit(0)
